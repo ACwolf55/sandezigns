d68ac68f701895e728d594600f3ed520cb569ef7
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2024-07-18 19:18:29 -0400

    frist

---
+++ src/Components/Artwork.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Artwork = () => {
+  return (
+    <section>
+    <h3>Artwork</h3>
+    </section>
+  )
+}
---
+++ src/Components/Blog.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Blog = () => {
+  return (
+    <section>
+    <h3>Blog</h3>
+    </section>
+  )
+}
